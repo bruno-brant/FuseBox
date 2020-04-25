0f7e6312bfcc9e796a3d6566c31730b7c0ea3a78
Bruno Brant <1890900+bruno-brant@users.noreply.github.com>
2020-04-25 12:07:03 -0300

    chore: corrects headers and inner docs

--- src/FuseBox/UnresolvableTypeException.cs
+++ src/FuseBox/UnresolvableTypeException.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) Bruno Brant. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
 using System.Runtime.Serialization;
 
 namespace FuseBox
@@ -39,7 +42,23 @@ namespace FuseBox
             Type = type;
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnresolvableTypeException"/> class with serialized data.
+        /// </summary>
+        /// <param name="info">
+        ///     The System.Runtime.Serialization.SerializationInfo that holds the serialized
+        ///     object data about the exception being thrown.
+        /// </param>
+        /// <param name="context">
+        ///     The System.Runtime.Serialization.StreamingContext that contains contextual information
+        ///     about the source or destination.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///     The info parameter is null.
+        /// </exception>
+        /// <exception cref="System.Runtime.Serialization.SerializationException">
+        ///     The class name is null or System.Exception.HResult is zero (0).
+        /// </exception>
         protected UnresolvableTypeException(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
